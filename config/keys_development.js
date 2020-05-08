f0f6bc77baa4c477409f1f14bb0f1b49403b2cc9
Anup16127 <62927616+Anup16127@users.noreply.github.com>
2020-05-08 14:54:40 +1000

    Update keys_development.js

--- config/keys_development.js
+++ config/keys_development.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGO_URI: 'mongodb+srv://saurav1:saurav1@cluster0-yeblu.mongodb.net/test?retryWrites=true&w=majority',
+    MONGO_URI: 'mongodb+srv://anup:anupkattel@cluster0-lf3l8.mongodb.net/test?retryWrites=true&w=majority',
     //MONGO_URI: 'mongodb://localhost/project',
     SECRET: 'paniparyo'
 };
